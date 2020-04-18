1262b1a9be2fb674c09006c2cab9a22eb0afcbfc
omn4ma <omn4ma@gmail.com>
2020-04-19 00:53:14 +0300

    add test cases

--- T9Converter.Tests/ConverterTests.cs
+++ T9Converter.Tests/ConverterTests.cs
@@ -6,8 +6,10 @@ namespace T9Converter.Tests
     public class ConverterTests
     {
         [Theory]
+        [InlineData("4", "")]
         [InlineData("yes", "999337777")]
         [InlineData("foo  bar", "333666 6660 022 2777")]
+        [InlineData("hello world", "4433555 555666096667775553")]
         public void Convert_ToT9String_CompareResult(string text, string t9codes)
         {
             var converter = GetConverter();

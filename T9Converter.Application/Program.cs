46fae7528011a198523d70de2ff162cc214bb3fa
omn4ma <omn4ma@gmail.com>
2020-04-19 10:54:57 +0300

    app

---
+++ T9Converter.Application/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using T9Converter.Domain;
+
+namespace T9Converter.Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String text;
+            int count = 0;
+            var converter = new Converter(Keyboards.Default);
+
+            Console.WriteLine($"Please enter text to convert it into a sequence of button presses for T9 spelling (or empty string to exit):");
+
+            do
+            {
+                text = Console.ReadLine();
+                var t9codes = converter.ToT9Codes(text);
+
+                if (!String.IsNullOrEmpty(t9codes))
+                {
+                    count++;
+                    Console.WriteLine($"Case #{count}: {t9codes}");
+                }
+
+            } while (text != String.Empty);
+
+            Console.WriteLine($"Closing");
+        }
+    }
+}
